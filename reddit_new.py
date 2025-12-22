5b9cb7fd30dcb59d8ec4c0dd6f04b76e82227474
samriddhi singh <samriddhi@samriddhis-MacBook-Air.local>
2025-12-22 19:02:01 +0530

    Add post count logging

--- reddit_new.py
+++ reddit_new.py
@@ -18,17 +18,17 @@ def fetch_post():
     with open("post.csv","w",encoding="utf-8",newline="") as csvfile:
         writter = csv.writer(csvfile)
         # writter.writerow(["title","score","created_utc","readable_time","comment_1","comment_2","comment_3"])
-
+        count = 0
         for post in subreddit.hot(limit=10):
             post.comments.replace_more(limit=0)
             top_comments = [c.body.replace("\n"," ") for c in post.comments[:3]]
             # if fewer exist
             while len(top_comments)<3:
                 top_comments.append("")
-
+            count+=1
             readable = datetime.fromtimestamp(post.created_utc).strftime("%Y-%m-%d %H:%M:%S")
             writter.writerow([post.id,post.title, post.score, post.created_utc,readable,top_comments[0],top_comments[1],top_comments[2]])
             print("Fetched and saved post:",post.title)
+        print("Total number of posts fetched: ",count)
 
 fetch_post()
-
